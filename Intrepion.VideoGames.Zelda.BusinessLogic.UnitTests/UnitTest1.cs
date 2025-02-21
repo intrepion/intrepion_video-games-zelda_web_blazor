b88dbd9db1850ee65df561f6c48dde78d48cfb3f
Oliver Forral <intrepion@gmail.com>
2025-02-20 19:58:54 -0700

    dotnet format

---
+++ Intrepion.VideoGames.Zelda.BusinessLogic.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Intrepion.VideoGames.Zelda.BusinessLogic.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
