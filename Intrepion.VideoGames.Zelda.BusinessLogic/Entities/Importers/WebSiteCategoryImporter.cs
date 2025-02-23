1f8fbf74c41d5777f7959e8d634f3259dcc76b78
Oliver Forral <intrepion@gmail.com>
2025-02-23 03:14:58 -0700

    dotnet format

--- Intrepion.VideoGames.Zelda.BusinessLogic/Entities/Importers/WebSiteCategoryImporter.cs
+++ Intrepion.VideoGames.Zelda.BusinessLogic/Entities/Importers/WebSiteCategoryImporter.cs
@@ -51,7 +51,7 @@ public static class WebSiteCategoryImporter
             // ManyToOneCodePlaceholder
 
             if (true
-                // NullCheckCodePlaceholder
+            // NullCheckCodePlaceholder
             )
             {
                 var webSiteCategory = new WebSiteCategory
