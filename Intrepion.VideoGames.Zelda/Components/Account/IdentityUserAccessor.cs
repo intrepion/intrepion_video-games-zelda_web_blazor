07ac0da2f8d0a4d4efa7ef5c1401b587ce477c4d
Oliver Forral <intrepion@gmail.com>
2025-02-27 01:50:39 -0700

    dotnet format

---
+++ Intrepion.VideoGames.Zelda/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,19 @@
+﻿using Intrepion.VideoGames.Zelda.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace Intrepion.VideoGames.Zelda.Components.Account;
+
+internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+{
+    public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+    {
+        var user = await userManager.GetUserAsync(context.User);
+
+        if (user is null)
+        {
+            redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+        }
+
+        return user;
+    }
+}
---
+++ Intrepion.VideoGames.Zelda/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intrepion.VideoGames.Zelda.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+}
