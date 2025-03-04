d1128f5a1354bfd166c052b3f34ea9973b597bd6
Oliver Forral <intrepion@gmail.com>
2025-03-04 16:11:42 -0700

    dotnet format

---
+++ Intrepion.VideoGames.Zelda.Server.ControllerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Intrepion.VideoGames.Zelda.Server.ControllerTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
